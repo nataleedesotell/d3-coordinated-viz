dbf010c7a2dc6ebd8f7cc71f9e9e012efca81206
nataleedesotell <nataleedesotell@gmail.com>
2016-03-30 09:12:32 -0500

    updates, map shows up now
    updates, map shows up now

--- js/main.js
+++ js/main.js
@@ -1,51 +1,53 @@
-
-//wrap everything in a self-executing anonymous function to move to local scope
-(function(){
-
-//begin script when window loads
+//start script when the window loads
 window.onload = setMap();
+
 //set up choropleth map
 function setMap(){
-    //map frame dimensions
-    var width = 960,
-        height = 650;
-    //create new svg container for the map
-    var map = d3.select("body")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+  //map frame dimensions
+  var width = 1000,
+    height = 900;
 
-    //create Albers equal area conic projection for the United States
-    var projection = d3.geo.albers()
-        .center([44, -90])
-        .parallels([43, 46])
-        .scale(2000)
-        //offsets pixel coordinates of the projection's center in the svg container
-        .translate([width / 2, height / 2])
+  //create new svg container for the map
+  var map = d3.select("body")
+    .append("svg")
+    .attr("class", "map")
+    .attr("width", width)
+    .attr("height", height);
 
-    var path = d3.geo.path()
-        .projection(projection);
+    //create Albers equal area conic projection
+  var projection = d3.geo.albers()
+  //set a center (thought this should be 44, -89?)
+  .center([6.1, 45.8])
+  //not sure what rotate does exactly
+  //.rotate([+38, 0])
+  //parallels -- not sure if these are right
+  .parallels([44, 46])
+  //scale -- sizes the map
+  .scale(10000)
+  // .translate([width / 480, height / 250]);
 
-    //use queue.js to parallelize asynchronous data loading
-    d3_queue.queue()
-        .defer(d3.csv, "data/County_Classifications.csv") //load attributes from csv
-        .defer(d3.json, "data/WI_Counties.topojson") //load choropleth spatial data
-        .await(callback);
+  //create a path generator
+  var path = d3.geo.path()
+    .projection(projection);
 
-    function callback(error, csvData, WI){
-        //translate the Counties topojson
-        var WICounties = topojson.feature(WI, WI.objects.Counties);
-        console.log(WICounties);
-        console.log(error);
-        console.log(csvData);
-        console.log("callback");
-        //add WICounties to the map
-        var counties = map.append("path")
-          .datum(WICounties)
-          .attr("class", "counties")
-          .attr("d", path);
+  //use queue.js to parallelize asynchronous data loading
+  d3_queue.queue()
+    .defer(d3.csv, "data/County_Classifications.csv") //load attributes from csv
+    .defer(d3.json, "data/WI_Counties.topojson") //load choropleth spatial data
+    .await(callback);
+
+  function callback(error, csvData, wi){
+    //translate wisconsin TopoJSON
+    var wiCounties = topojson.feature(wi, wi.objects.WI_Counties).features;
+    console.log(wiCounties)
+    //add Wisconsin counties to map
+    var counties = map.selectAll(".counties")
+        .data(wiCounties)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "counties" + d.properties.CTY_FIPS;
+        })
+        .attr("d", path);
     };
 };
-
-})();
