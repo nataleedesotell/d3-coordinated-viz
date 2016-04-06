f8f4e16bc6d6ee194f364253058af6bd87a4f925
nataleedesotell <nataleedesotell@gmail.com>
2016-04-06 12:17:16 -0500

    Module 9 completed
    Module 9 completed -- title positioned a little better, axis added,
    colors adjusted

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+//First line of  code wraps everything in a self-executing anonymous function to move to local scope
 (function(){
 //pseudo-global variables
+//my list of attributes (rurality classifications by county)
 var attrArray = ["RUCC2013", "URCSC2013", "UIC2013", "IRR2010", "IRR2010Category", "NCFC2010", "CBSA2013"];
 var expressed = attrArray[0]; //initial attribute
 //begin script when window loads
 window.onload = setMap();
-//set up choropleth map
+//set up choropleth 
 function setMap() {
     //map frame dimensions
-    var width = window.innerWidth * 0.6,
+    var width = window.innerWidth * 0.9,
         height = 900;
-  //create new svg container for the map
+  //create svg container for the map
   var map = d3.select("body")
     .append("svg")
     .attr("class", "map")
@@ -18,14 +19,12 @@ function setMap() {
     .attr("height", height);
   //create Albers equal area conic projection
   var projection = d3.geo.albers()
-  //set a center 
+  //set a center/rotation to focus on WI
   .center([-0.00, 44.437778])
-  //not sure what rotate does exactly
   .rotate([90.130186, 0, 0])
-  //parallels -- not sure if these are right
   .parallels([42, 46])
   //scale -- sizes the map
-  .scale(9000)
+  .scale(8500)
   .translate([width / 2, height / 2]);
   //create a path generator
   var path = d3.geo.path()
@@ -35,8 +34,9 @@ function setMap() {
   d3_queue.queue()
     .defer(d3.csv, "data/County_Classifications.csv") //load attributes from csv
     .defer(d3.json, "data/WI_Counties.topojson") //load choropleth spatial data
-    //firess when all the data is loaded, sends all data to the callback function
+    //fires when all the data is loaded, sends all data to the callback function
     .await(callback);
+
 //set up callback function with 3 parameters within setMap() so it can use variables above
   function callback(error, csvData, wi){
     //translate WI TopoJSON using the topojson.feature() method
@@ -44,13 +44,15 @@ function setMap() {
     wiCounties = joinData(wiCounties, csvData);
     //add color scale
     var colorScale = makeColorScale(csvData);
+    //add enumeration units to the map
     setEnumerationUnits(wiCounties, map, path, colorScale);
     //add coordinated viz to the map
     setChart(csvData, colorScale);
     };
-};
+};//end setMap()
 
 function joinData(wiCounties, csvData) {
+    //loop through csv to assign csv attribute values to the counties
     for (var i=0; i<csvData.length; i++){
         var csvCounty = csvData[i]; //the current county
         var csvKey = csvCounty.County; //the CSV primary key
@@ -58,8 +60,6 @@ function joinData(wiCounties, csvData) {
         for (var a=0; a<wiCounties.length; a++){
             var geojsonProps = wiCounties[a].properties; //the current region geojson properties
             var geojsonKey = geojsonProps.NAME; //the geojson primary key
-            // console.log(geojsonKey);
-            // console.log(csvKey);
             //where primary keys match, transfer csv data to geojson properties object
             if (geojsonKey == csvKey){
                 //assign all attributes and values
@@ -132,12 +132,12 @@ function setChart(csvData, colorScale){
         d3.max(csvData, function(d) {
             return parseFloat(d[expressed]); })
     ];
-    //chart frame dimensions
+    //chart frame dimensions with padding (0 now but will adjust later)
     var chartWidth = window.innerWidth * 0.9,
-        chartHeight = 400,
-        leftPadding = 40,
-        rightPadding = 10,
-        topBottomPadding = 10,
+        chartHeight = 500,
+        leftPadding = 0,
+        rightPadding = 0,
+        topBottomPadding = 0,
         chartInnerWidth = chartWidth - leftPadding - rightPadding
         chartInnerHeight = chartHeight - topBottomPadding * 2,
         translate = "translate(" +leftPadding + "," + topBottomPadding + ")";
@@ -147,7 +147,6 @@ function setChart(csvData, colorScale){
         .attr("width", chartWidth)
         .attr("height", chartHeight)
         .attr("class", "chart");
-//make a rectangle for the background of the chart
 
     //scale the bar sizes proportionally 
     var yScale = d3.scale.linear()
@@ -178,25 +177,53 @@ function setChart(csvData, colorScale){
         .style("fill", function(d){
             return choropleth(d, colorScale);
         });
-    //annotate bars with attribute value text
+
+       //create a title for my chart
+      var chartTitle = chart.append("text")
+          .attr("x", 30)
+          .attr("y", 150)
+          .attr("class", "chartTitle")
+          .text("Rurality of Counties in the " + expressed[1] + " Classification System");
+
+    //creates vertical axis generator
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+
     //places axis on the chart
     var axis = chart.append("g")
         .attr("class", "axis")
         .attr("transform", translate)
         .call(yAxis);
-       //create a title for my chart
-      var chartTitle = chart.append("text")
-          .attr("x", 20)
-          .attr("y", 50)
-          .attr("class", "chartTitle")
-          .text("Rurality of Counties in the " + expressed[1] + " Classification System");
 
-    //frame for chart 
-    var chartFrame = chart.append("rect")
-        .attr("class", "chartFrame")
-        .attr("width", chartInnerWidth)
-        .attr("height", chartInnerHeight)
-        .attr("transform", translate);
+    //keeping this code in case I decide to use numbers instead
+    // var numbers = chart.selectAll(".numbers")
+    //     .data(csvData)
+    //     .enter()
+    //     .append("text")
+    //     .sort(function(a, b){
+    //         return a[expressed]-b[expressed]
+    //     })
+    //     .attr("class", function(d){
+    //         return "numbers " + d.adm1_code;
+    //     })
+    //     .attr("text-anchor", "middle")
+    //     .attr("x", function(d, i){
+    //         var fraction = chartWidth / csvData.length;
+    //         return i * fraction + (fraction - 1) / 2;
+    //     })
+    //     .attr("y", function(d){
+    //         return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+    //     })
+    //     .text(function(d){
+    //         return d[expressed];
+    //     });
+    // //frame for chart 
+    // var chartFrame = chart.append("rect")
+    //     .attr("class", "chartFrame")
+    //     .attr("width", chartInnerWidth)
+    //     .attr("height", chartInnerHeight)
+    //     .attr("transform", translate);
 
 };
 
